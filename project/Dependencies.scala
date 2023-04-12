beb19215368c5a8a6d1908ee0e590d9ef70c46b6
Richard Thomas <richard.thomas36@gmail.com>
2023-04-12 08:27:04 +0200

    checkpoint

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,4 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val munit = "org.scalameta" %% "munit" % "0.7.29"
+  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.4.8"
+  lazy val cats = "org.typelevel" %% "cats-core" % "2.9.0"
 }
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,39 @@
 package example
 
-object Hello extends Greeting with App {
-  println(greeting)
-}
+import cats.effect.kernel.MonadCancel
+import cats.effect.{ExitCode, IO, IOApp}
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+import java.io.{BufferedReader, InputStream, InputStreamReader, ObjectInputStream}
+import java.net.ServerSocket
+
+object Hello extends IOApp {
+  val listener: ServerSocket = new ServerSocket(9080)
+  def run(args: List[String]): IO[ExitCode] = {
+    for {
+     server <- createServer().foreverM.start.whileM_(IO(true))
+      _ <- IO.println("richard")
+    } yield (ExitCode.Success)
+  }
+
+
+  private def createServer() = {
+    IO {
+      val dd = listener.accept()
+      val in = // 3rd statement
+        new BufferedReader(
+          new InputStreamReader(dd.getInputStream()));
+      println("user joined")
+      while (true) {
+        println("ri")
+        val z = in.readLine()
+        println(z)
+        println("zc")
+      }
+    }
+  }
+  private def block(listener: ServerSocket) = {
+    listener.accept()
+    println("user connected")
+  }
+
 }
